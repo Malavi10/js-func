afaecbf1d8ac08a43f31b88ed3d8e96ad453894c
Malavi10 <131436564+Malavi10@users.noreply.github.com>
2023-05-31 12:47:36 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,30 @@
+
+
+function multiply(num1, num2){
+    var num1 = 23;
+    var num2 = 32;
+    result = num1*num2;
+    document.getElementById('pro').innerHTML = result;
+
+}
+
+function add(num1, num2){
+    var num1 = 23;
+    var num2 = 32;
+    result1 = num1 + num2;
+    document.getElementById('sum').innerHTML = result1;
+}
+
+function subtract(num1, num2){
+    var num1 = 23;
+    var num2 = 32;
+    result2 = num1 - num2;
+    document.getElementById('sub').innerHTML = result2;
+}
+
+function divide(num1, num2){
+    var num1 = 23;
+    var num2 = 32;
+    result2 = num1 / num2;
+    document.getElementById('divi').innerHTML = result2;
+}
